3819514beaec35b481769c4b0f32def2b89885e8
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-06-20 00:50:02 -0400

    Sample answer for Array Exercise Q1

---
+++ Exercises/ArraysExercises.js
@@ -0,0 +1,6 @@
+//Q1
+let groceryList = ["rice", "tea", "cheese", "soy sauce", "tomato sauce"];
+let numItems = groceryList.length;
+
+console.log(`Please buy ${numItems} items from the grocery store.`);
+console.log(`Prioritize buying ${groceryList[numItems - 1]} and ${groceryList[numItems - 2]}.`);
