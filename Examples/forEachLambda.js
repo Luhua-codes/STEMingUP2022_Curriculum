07c5973a88d1a9b534e9daa0930d2beb77dd9f34
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-06-24 23:44:47 -0400

    explain forEach function call

--- Examples/forEachLambda.js
+++ Examples/forEachLambda.js
@@ -18,3 +18,11 @@ drinks.forEach(myDrink => {
     totalSips++;
     console.log(`Took a sip of ${myDrink}. ${totalSips} sips taken in total.`);
 });
+console.log(); //newline
+
+//If more than one parameter needed, declare an in-line function:
+drinks.forEach(function (myDrink, index){
+    // forEach can call function and provide up to 3 arguments: current item, current index, and the array.
+    // Here, only the current item (myDrink) and the current index (index) are being used.
+    console.log(`Took a sip of ${myDrink}. ${index + 1} sips taken in total.`); //+1 to index because indexing starts from 0
+});
