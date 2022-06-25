b608967baedb7c63510d4cd2c2f3df8b4f64a8a0
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-06-24 23:29:23 -0400

    Add multiple line labmda to lambda example and fix console formatting

--- Examples/forEachLambda.js
+++ Examples/forEachLambda.js
@@ -1,10 +1,20 @@
 const drinks = ["coffee", "tea", "latte", "juice"];
 
 //This:
-drinks.forEach(sip);
 function sip(myDrink){
-    console.log("Took a sip of " + myDrink);
+    console.log(`Took a sip of ${myDrink}.`);
 }
 
+drinks.forEach(sip);
+console.log(); //newline
+
 //Is the same as doing this:
-drinks.forEach(myDrink => console.log("Took a sip of " + myDrink));
+drinks.forEach(myDrink => console.log(`Took a sip of ${myDrink}.`));
+console.log(); //newline
+
+//If lambda function is more than one line, use curly brackets:
+let totalSips = 0;
+drinks.forEach(myDrink => {
+    totalSips++;
+    console.log(`Took a sip of ${myDrink}. ${totalSips} sips taken in total.`);
+});
