2efcd7259a9026796ffdc81c5a58b703e80befc8
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-06-24 22:41:11 -0400

    upadte slice example to include index out of bounds case

--- Examples/spliceExample.js
+++ Examples/spliceExample.js
@@ -1,10 +1,21 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+//will automatically insert "new fruit" at the next array index, instead of leaving undefined holes:
+fruits.splice(7, 0, "new fruit"); //"new fruit" will be at index 4
+
+//insert at index 2 and remove 2 elements
 fruits.splice(2, 2, "Lemon", "Kiwi");
+
 console.log(fruits);
-fruits.splice(7, 0, "new fruit"); //indicated index out of bounds. Will not cause error, but will not have any effect on the array.
 
 /*
-* Values at the end: [ 'Banana', 'Orange', 'Lemon', 'Kiwi' ]
+* Values at the end: [ 'Banana', 'Orange', 'Lemon', 'Kiwi', 'new fruit' ]
+* "new fruit" was added at index 4
 * 2 elements were deleted, starting at index 2, so "Apple" and "Mango" were removed
-* New elements "Lemon" and "Kiwi" were inserted at index 2, where "Apple" used to be
+* New elements "Lemon" and "Kiwi" were inserted at index 2, after "Orange" and before "new fruit"
 */
+
+fruits.splice(1, 2); //delete "Orange" and "Lemon" without leaving undefined holes
+console.log(fruits);
+
+//remaining elements: [ 'Banana', 'Kiwi', 'new fruit' ]
