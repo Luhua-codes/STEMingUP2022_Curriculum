8cc210cafe190ac056c7a6a8e08e9340510e43b7
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-06-29 01:48:57 -0400

    Inheritance and private attributes

--- Examples/advancedClassesExample.js
+++ Examples/advancedClassesExample.js
@@ -1,9 +1,5 @@
 class Chair {
-    static countChairs = 0; //keeps track of the total number of chairs
-
     constructor(colour, seatSize, seatHeight) {
-        Chair.countChairs++; //whenever a new chair is made, increase the counter
-
         this.colour = colour;
         this.seatSize = seatSize; //inches
         this.seatHeight = seatHeight; //inches
@@ -22,11 +18,25 @@ class Chair {
 
     moveChair() {
     };
+
+    //private method
+    static #calculateTotalPrice(taxPercent, price, countChairs){
+        return (price + (price * taxPercent / 100)) * countChairs;
+    }
+
+    static generateBill(price, type, countChairs){
+        console.log("***BILLING INVOICE***");
+        console.log(`Total due: $${Chair.#calculateTotalPrice(13, price, countChairs)} for ${countChairs} ${type}s.`);
+    }
 }
 
 class OfficeChair extends Chair {
+    static countOfficeChairs = 0; //keeps track of the total number of office chairs
+    static #price = 200; //private variable
+
     constructor(colour, seatSize, seatHeight) { //overloaded constructor with default values assigned
         super(colour, seatSize, seatHeight); //calls the constructor belonging to the superclass (Chair)
+        OfficeChair.countOfficeChairs++; //whenever a new office chair is made, increase the counter
         this.type = "Office Chair"; //new property, identifies the type of chair
 
         //default values that are different for an office chair
@@ -48,8 +58,12 @@ class OfficeChair extends Chair {
     moveChair(x, y) {
         console.log(`Chair moved to coordinates (${x}, ${y}).`);
     };
+
+    static generateBill() {
+        super.generateBill(this.#price, "Office Chair", this.countOfficeChairs);
+    }
 }
 
 const officeChair0 = new OfficeChair("Black", 15, 12);
-console.log(officeChair0);
-console.log(`There are ${Chair.countChairs} chair(s) in the room.`);
+const officeChair1 = new OfficeChair("Red", 20, 15);
+OfficeChair.generateBill();
