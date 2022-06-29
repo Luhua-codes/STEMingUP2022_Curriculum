3d3d37aed9e26217a598848d3ab994ea5f033d9a
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-06-28 21:45:47 -0400

    Add ternary operator examples and switch case example

--- Test.js
+++ Test.js
@@ -1,1 +1,11 @@
 console.log(`1 + 2 = ${1 + 2}`);
+
+const person0 = {
+  firstName: "Alice",
+  lastName: "Smith",
+  age: 20,
+  location: "Toronto, Canada"
+};
+
+person0["fullName"] = person0.firstName + person0.lastName;
+console.log(person0);
