1dbef948172e9c23c001f8dc891461463bd04391
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-07-08 00:42:46 -0400

    DOM examples

--- DOM/DOMexamples.js
+++ DOM/DOMexamples.js
@@ -1,31 +1,43 @@
 let kardImg = document.getElementById("KARD photo"); //access by ID
 
-function imgSwap() { //called by onclick attribute of change image button
+document.getElementById("imgSwap").onclick = function () { //add onclick attribute to imgSwap button
+    kardImg = document.getElementById("KARD photo");
+    let newImg = document.createElement("img");
+    newImg.setAttribute("id", "KARD photo");
+
     //when button pressed, change to the other image
     if (kardImg.getAttribute("src") === "KARD M Countdown.webp") { //use getAttribute to see what the current src is
-        kardImg.setAttribute("src", "KARD Ride on the Wind.webp"); //use setAttribute to assign a new value to src
-        kardImg.setAttribute("alt", "KARD Ride on the Wind");
+        newImg.setAttribute("src", "KARD Ride on the Wind.webp"); //use setAttribute to assign a new value to src
+        newImg.setAttribute("alt", "KARD Ride on the Wind");
     } else {
-        kardImg.setAttribute("src", "KARD M Countdown.webp");
-        kardImg.setAttribute("alt", "KARD M Countdown");
+        newImg.setAttribute("src", "KARD M Countdown.webp");
+        newImg.setAttribute("alt", "KARD M Countdown");
     }
 
+    //replace the current image with newImg
+    kardImg.replaceWith(newImg);
     //update innerText of photo caption to match current image
     document.getElementById("KARD photo caption").innerText = kardImg.getAttribute("alt");
 }
 
-function imgShrink(){ //called by onclick attribute of shrink image button
+document.getElementById("imgShrink").onclick = function () { //called by onclick attribute of shrink image button
+    kardImg = document.getElementById("KARD photo");
     kardImg.setAttribute("width", (kardImg.width - 5).toString());
 }
 
-function imgGrow(){
+document.getElementById('imgGrow').onclick = function () {
+    kardImg = document.getElementById("KARD photo");
     kardImg.setAttribute("width", (kardImg.width + 5));
 }
 
-function memberAdd(){
+function memberAdd() {
     const kardMembers = ['J.Seph', 'BM', 'Somin', "Jiwoo"];
     let addText = document.createElement("p"); //create a new paragraph element
-    let member = document.createTextNode(kardMembers[Math.floor(Math.random() * 4)]); //create a text node to be used as the innerText of the <p>
+
+    //create a text node to be used as the innerText of the <p>
+    //choose a random member to add (randomly choose an array index)
+    let member = document.createTextNode(kardMembers[Math.floor(Math.random() * 4)]);
+
     addText.appendChild(member); //append the member textNode to the addText paragraph
 
     document.getElementById("Members List").appendChild(addText); //append the addText paragraph to the Members List div
