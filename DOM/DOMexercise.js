a91cd67b0c8eaad13a98dd8cd2bb58ccbd84a864
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-07-08 01:43:04 -0400

    Q2 Solution

--- DOM/DOMexercise.js
+++ DOM/DOMexercise.js
@@ -6,3 +6,8 @@ document.getElementById("submitButton").onclick = function (){
     document.getElementById("emailSubmitted").innerText = userEmail;
     document.getElementById("passwordSubmitted").innerText = userPassword;
 }
+
+//Q2
+document.getElementById("changeBG").onclick = function (){
+    document.body.style = "background: " + '#'+Math.floor(Math.random()*16777215).toString(16);
+}
