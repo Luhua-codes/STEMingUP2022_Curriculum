6396f8742460de8a6b11e0e90d5dff2a2a1f9a76
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-07-29 13:04:47 -0400

    comment out Button

--- ReactBootstrap/react-bootstrap/src/App.js
+++ ReactBootstrap/react-bootstrap/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button>Test</Button>
+        {/*<Button>Test</Button>*/}
         <a
           className="App-link"
           href="https://reactjs.org"
